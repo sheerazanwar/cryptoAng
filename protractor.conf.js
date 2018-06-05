c8421aadf3105a965d665ca26c015752114a520d
Sheeraz Anwar <sheeraz.anwar@outlook.com>
2018-06-05 11:33:48 +0500

    logs removed

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://cryptoangular.herokuapp.com',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
